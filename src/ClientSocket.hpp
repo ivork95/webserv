7a24cd36df14c0079e2d8829e76e56c81b11d9e0
Kawish Gajadien <kawishg@outlook.com>
2023-08-08 15:45:42 +0200

    feat: put accept into ClientSocket constructor + add outstream overload for ClientSocket + edit names main

--- src/ClientSocket.hpp
+++ src/ClientSocket.hpp
@@ -11,22 +11,45 @@ public:
     socklen_t m_clientAddrSize{sizeof(m_clientAddr)};
 
     // default constructor
-    ClientSocket(void);
+    ClientSocket(void) = delete;
+
+    // serverSocket constructor
+    ClientSocket(int serverSocket);
 
     // destructor
     ~ClientSocket(void);
+
+    // outstream operator overload
+    friend std::ostream &operator<<(std::ostream &out, const ClientSocket &clientsocket);
 };
 
-ClientSocket::ClientSocket(void)
+// serverSocket constructor
+ClientSocket::ClientSocket(int serverSocket)
 {
-    std::cout << "ClientSocket constructor called\n";
+    m_clientSocket = accept(serverSocket, (struct sockaddr *)&(m_clientAddr), &(m_clientAddrSize));
+    if (m_clientSocket == -1)
+    {
+        std::perror("accept() failed");
+        throw std::runtime_error("Error: accept() failed\n");
+    }
+
+    std::cout << *this << " constructor called\n";
 }
 
+// destructor
 ClientSocket::~ClientSocket(void)
 {
-    std::cout << "ClientSocket destructor called\n";
+    std::cout << *this << " destructor called\n";
 
     close(m_clientSocket);
+}
+
+// outstream operator overload
+std::ostream &operator<<(std::ostream &out, const ClientSocket &clientsocket)
+{
+    out << "ClientSocket(" << clientsocket.m_clientSocket << ", " << clientsocket.m_clientAddr.sin_family << ", " << ntohs(clientsocket.m_clientAddr.sin_port) << ", " << ntohl(clientsocket.m_clientAddr.sin_addr.s_addr) << ')';
+
+    return out;
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -13,31 +13,28 @@ void handleSigint(int)
 
 int main(void)
 {
-    ServerIO s{};
+    ServerIO serverio{};
     int nReadyFds{};
     char buffer[BUFSIZE]{};
     ssize_t nBytesReceived{};
     int currEventFd{};
     unsigned int args[2]{12345, 23456};
-    // Used to simulate a Python for-else loop
-    bool flag{false};
+    bool isLoopBroken{false};
 
     std::signal(SIGINT, handleSigint);
 
-    // Create TcpServer instance(s) for each port
     for (auto &arg : args)
     {
-        std::unique_ptr<TcpServer> t = std::make_unique<TcpServer>(arg);
-        s.m_servers.push_back(std::move(t));
+        std::unique_ptr<TcpServer> tcpserver = std::make_unique<TcpServer>(arg);
+        serverio.m_servers.push_back(std::move(tcpserver));
     }
 
-    // Loop over TcpServer instance(s) and add to EpollFd
-    for (auto &server : s.m_servers)
-        s.addSocketToEpollFd(server->m_serverSocket);
+    for (auto &server : serverio.m_servers)
+        serverio.addSocketToEpollFd(server->m_serverSocket);
 
     while (true)
     {
-        nReadyFds = epoll_wait(s.m_epollFD, s.m_events.data(), MAX_EVENTS, (3 * 60 * 1000));
+        nReadyFds = epoll_wait(serverio.m_epollFD, serverio.m_events.data(), MAX_EVENTS, (3 * 60 * 1000));
         if (nReadyFds == -1)
         {
             if (errno == EINTR && caughtSigint)
@@ -54,32 +51,23 @@ int main(void)
 
         for (int i{0}; i < nReadyFds; i++)
         {
-            currEventFd = s.m_events.at(i).data.fd;
-            flag = false;
-            for (auto &server : s.m_servers)
+            isLoopBroken = false;
+            currEventFd = serverio.m_events.at(i).data.fd;
+            for (auto &server : serverio.m_servers)
             {
-                // Loop over TcpServer instance(s)
                 if (currEventFd == server->m_serverSocket)
                 {
-                    // This is an event from a TcpServer instance, aka a new client requesting connection to this instance
-                    std::unique_ptr<ClientSocket> c = std::make_unique<ClientSocket>();
-                    c->m_clientSocket = accept(server->m_serverSocket, (struct sockaddr *)&(c->m_clientAddr), &(c->m_clientAddrSize));
-                    if (c->m_clientSocket == -1)
-                    {
-                        std::perror("accept() failed");
-                        throw std::runtime_error("Error: accept() failed\n");
-                    }
-                    s.addSocketToEpollFd(c->m_clientSocket);
-                    server->m_clientSockets.push_back(std::move(c));
+                    std::unique_ptr<ClientSocket> clientsocket = std::make_unique<ClientSocket>(server->m_serverSocket);
 
-                    flag = true;
+                    serverio.addSocketToEpollFd(clientsocket->m_clientSocket);
+                    server->m_clientSockets.push_back(std::move(clientsocket));
+
+                    isLoopBroken = true;
                     break;
                 }
             }
 
-            if (!flag)
-            // Loop over TcpServer instance(s) was completed without finding a match
-            // This means the event came from an already connected client socket
+            if (!isLoopBroken)
             {
                 nBytesReceived = recv(currEventFd, buffer, BUFSIZE, 0);
                 if (nBytesReceived < 0)
@@ -87,20 +75,17 @@ int main(void)
                 else if (nBytesReceived == 0)
                 {
                     std::cout << "Client disconnected before sending data.\n";
-                    s.deleteSocketFromEpollFd(currEventFd);
+                    serverio.deleteSocketFromEpollFd(currEventFd);
                 }
                 else
                 {
-                    // Null-terminate the received data in case it's a string
                     if (nBytesReceived < BUFSIZE)
                         buffer[nBytesReceived] = '\0';
                     else
                         buffer[BUFSIZE - 1] = '\0';
 
-                    // Print the received data
                     std::cout << "Received from client: " << buffer << "\n";
 
-                    // Echo the data back to the client
                     if (send(currEventFd, buffer, nBytesReceived, 0) != nBytesReceived)
                         std::cerr << "Error: send() failed\n";
                 }
