a0f665d32f59392dbc8cee23f4c50d2a9c176a5a
idan vork <idanvork@gmail.com>
2023-08-23 01:13:08 +0200

    feat: Added httpMessage class

--- src/Client.hpp
+++ src/Client.hpp
@@ -7,16 +7,16 @@
 #include <fcntl.h>
 #include <arpa/inet.h>
 #include "Socket.hpp"
+#include "HttpMessage.hpp"
 
 class Client : public Socket
 {
 public:
-    std::string httpMessage{};
     struct sockaddr_storage m_remoteaddr
     {
     };
     socklen_t m_addrlen{sizeof(m_remoteaddr)};
-
+    HttpMessage requestMessage;
     // default constructor
     Client(void) = delete;
 
