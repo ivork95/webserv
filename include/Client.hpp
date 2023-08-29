7757bd308d82850b0d38e7c61a8668f38e20ddc6
Kawish Gajadien <kawishg@outlook.com>
2023-08-29 13:08:05 +0200

    fix: edit project to compile with third party logger

---
+++ include/Client.hpp
@@ -0,0 +1,40 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <iostream>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include "Socket.hpp"
+#include "HttpMessage.hpp"
+
+class Client : public Socket
+{
+public:
+    struct sockaddr_storage m_remoteaddr
+    {
+    };
+    socklen_t m_addrlen{sizeof(m_remoteaddr)};
+
+    // data only used to print info about socket
+    char m_ipstr[INET6_ADDRSTRLEN];
+    void *m_addr;
+    const char *m_ipver;
+    int m_port;
+
+    HttpMessage requestMessage;
+    // default constructor
+    Client(void) = delete;
+
+    // serverSocket constructor
+    Client(int serverSocket);
+
+    // destructor
+    ~Client(void);
+
+    // outstream operator overload
+    friend std::ostream &operator<<(std::ostream &out, const Client &clientsocket);
+};
+
+#endif
