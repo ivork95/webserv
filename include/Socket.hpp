e4e9e7697608c8e008a85a220c3565fe25841c17
Kawish Gajadien <kawishg@outlook.com>
2023-09-01 16:06:35 +0200

    chore: put info attributes in Socket

--- include/Socket.hpp
+++ include/Socket.hpp
@@ -3,11 +3,20 @@
 
 #include <fcntl.h>
 #include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 class Socket
 {
 public:
     int m_socketFd;
+
+    // data only used to print info about socket
+    char m_ipstr[INET6_ADDRSTRLEN];
+    void *m_addr;
+    const char *m_ipver;
+    int m_port;
+
     int setNonBlocking(void);
     virtual ~Socket(void)
     {
