697d205b663621cc40e0d8e97a392a6504b5bc88
Arthur Beznik <arthurbeznik@gmail.com>
2023-10-28 14:06:57 +0200

    Added tests + makefile rule + new conf file + testing files

--- tests/tests.py
+++ tests/tests.py
@@ -20,19 +20,27 @@ class TestServerResponse(unittest.TestCase):
         )
         self.assertEqual(response.status_code, 404)
 
-	# def test_get_method_not_allowed
-	# def test_get_method_not_allowed(self):
-		
+    def test_get_method_not_allowed(self):
+        url = "http://localhost:8081/no-get"
 
-	# def test_get_autoindex
+        response = requests.get(
+            url=url
+        )
+        self.assertEqual(response.status_code, 405)
 
+    def test_get_autoindex(self):
+        url = "http://localhost:8081/autoindex/"
+
+        response = requests.get(
+            url=url
+        )
+        self.assertEqual(response.status_code, 200)
+	
 	# def test_get_redirection
-
-	# def test_get_bigger_max_client_size
 
     def test_post(self):
         url = "http://localhost:8081/upload"
-        files = {'file': open('Poster.jpg', 'rb')}
+        files = {'file': open('tests/Poster.jpg', 'rb')}
         headers = {
             'Content-Length': '1046495',
         }
@@ -46,7 +54,36 @@ class TestServerResponse(unittest.TestCase):
         self.assertTrue(response.history)
         self.assertEqual(response.history[0].status_code, 303)
 
-	# def test_post_method_not_allowed
+	# NB I struggled to make a simple post request so I copied yours
+    def test_post_bigger_max_client_size(self):
+        url = "http://localhost:8081/post-bigger"
+        files = {'file': open('tests/Poster.jpg', 'rb')}
+        headers = {
+            'Content-Length': '1046495',
+        }
+
+        response = requests.post(
+            url=url,
+            headers=headers,
+            files=files,
+        )
+        files["file"].close()
+        self.assertEqual(response.status_code, 413)
+
+    def test_post_method_not_allowed(self):
+        url = "http://localhost:8081/no-post"
+        files = {'file': open('tests/Poster.jpg', 'rb')}
+        headers = {
+            'Content-Length': '1046495',
+        }
+
+        response = requests.post(
+            url=url,
+            headers=headers,
+            files=files,
+        )
+        files["file"].close()
+        self.assertEqual(response.status_code, 405)
 
     def test_post_chunked(self):
         conn = http.client.HTTPConnection("localhost:8081")
@@ -65,34 +102,51 @@ class TestServerResponse(unittest.TestCase):
         # print(response.status, response.reason)
         # print(response.read())
 
-	# def test_post_chunked_invalid_chunks
+	# This should be 400 Bad Request
+    def test_post_invalid_chunks(self):
+        conn = http.client.HTTPConnection("localhost:8081")
 
-    def atest_delete(self):
-        filename = '../www/upload/test.txt'
+        conn.putrequest("POST", "/upload")
+        conn.putheader("Transfer-Encoding", "chunked")
+        conn.endheaders()
 
-        try:
-            with open(filename, 'x') as file:
-                pass
-        except FileExistsError:
-            pass
+        conn.send(b"9\r\nMozilla\r\n11\r\nDeveloper Network\r\n0\r\n\r\n")
 
-        url = f"http://localhost:8081/upload/{filename}"
+        response = conn.getresponse()
+        conn.close()
+        self.assertEqual(response.status, 500)
+
+	# We check if the file exists and return an error if not
+    def test_delete(self):
+        filename = 'test.txt'
+        file_path = f"./www/delete/{filename}"
+
+        # Create the file
+        with open(file_path, 'w') as file:
+            file.write("Test content")
+
+        url = f"http://localhost:8081/delete/{filename}"
         response = requests.delete(
             url=url
         )
         self.assertEqual(response.status_code, 200)
 
     def test_delete_method_not_allowed(self):
-        url = "http://localhost:8081/images/test"
+        url = "http://localhost:8081/no-delete/test"
         response = requests.delete(
             url=url
         )
         self.assertEqual(response.status_code, 405)
 
-	# def test_delete_file_does_not_exist
+	# Should it be a 400 Bad Request ?
+    def test_delete_file_does_not_exist(self):
+        url = "http://localhost:8081/delete/does_not_exist"
+        response = requests.delete(
+            url=url
+        )
+        self.assertEqual(response.status_code, 500)
 
 	# def test_unknown_requet
-
 
 
 if __name__ == '__main__':
