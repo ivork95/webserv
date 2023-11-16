015a3a8e312e0968c1859cd13859882d193e0b20
Idan Vork <70711888+ivork95@users.noreply.github.com>
2023-11-16 17:06:53 +0100

    organize cgi

--- www/cgi-bin/hello.py
+++ www/cgi-bin/hello.py
@@ -1,12 +1,1 @@
-import os
-import sys
-
-if sys.stdin.isatty():
-    print("No input provided.")
-    sys.exit(0)
-# print("helllo")
-for line in sys.stdin:
-    if not line:
-        break
-    line = line.strip()
-    print(line)
+print("hello")
