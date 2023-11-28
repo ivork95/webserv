75021b03672f2287aa4969ba6aa76fe768801c74
Kawish Gajadien <kawish@jayzer.nl>
2023-11-28 11:33:03 +0100

    chore: cleanup

--- include/Token.hpp
+++ include/Token.hpp
@@ -7,34 +7,34 @@
 class Token
 {
 public:
-	enum ETokenType
-	{
-		WORD,
-		SEMICOLON,
-		OPEN_BRACE,
-		CLOSE_BRACE,
-		NEW_LINE,
-		NA
-	};
-	Token(void) = delete;
+    enum ETokenType
+    {
+        WORD,
+        SEMICOLON,
+        OPEN_BRACE,
+        CLOSE_BRACE,
+        NEW_LINE,
+        NA
+    };
+    Token(void) = delete;
 
-	// constructor
-	Token(Token::ETokenType &type);
+    // constructor
+    Token(Token::ETokenType &type);
 
-	// constructor
-	Token(std::string &word);
+    // constructor
+    Token(std::string &word);
 
-	// getters/setters
-	const Token::ETokenType &getType(void) const;
-	const std::string &getWord(void) const;
-	void setType(const Token::ETokenType &type);
-	void setWord(const std::string &word);
+    // getters/setters
+    const Token::ETokenType &getType(void) const;
+    const std::string &getWord(void) const;
+    void setType(const Token::ETokenType &type);
+    void setWord(const std::string &word);
 
-	friend std::ostream &operator<<(std::ostream &out, const Token &token);
+    friend std::ostream &operator<<(std::ostream &out, const Token &token);
 
 private:
-	ETokenType _type;
-	std::string _word;
+    ETokenType _type;
+    std::string _word;
 };
 
 #endif
